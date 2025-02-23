fa69b69b33680070a7a18270f197666791f89d26
Lan Ngai <b00159127@mytudublin.ie>
2025-02-23 21:08:17 +0000

    Copying from version 2.0

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ 	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title> Cat's Delight </title>
+    <link rel="stylesheet" href="index.css">
+</head>
+
+<body>
+<nav>
+    <div class="topnav">
+        <a class="active" href="index.php">Home</a>
+        <a href="reviews/reviews.php">Reviews</a>
+        <a href="products/products.php">Products</a>
+    </div>
+    <div>
+        <a href="login/login.php" style="float: right">Login</a>
+    </div>
+</nav>
+    <h1 class="title"> Cat's Delight </h1>
+
+    <h2> About us </h2>
+    <text class="description">Lorem ipsum odor amet, consectetuer adipiscing elit. Accumsan nam orci ac odio mattis cras integer. Fames mi litora fusce porttitor facilisis. Risus tellus cubilia natoque habitant convallis sem nisl. Pharetra magnis auctor mi neque convallis dictumst interdum. Felis fermentum turpis magna facilisi facilisis euismod dolor. Imperdiet ex risus aptent quam feugiat mus. Leo ad varius tempor; sollicitudin ligula potenti rutrum. Ridiculus sodales molestie ex ridiculus quam tincidunt cursus inceptos.</text>
+
+    <h2> Our Goal </h2>
+    <text class="description">Lorem ipsum odor amet, consectetuer adipiscing elit. Accumsan nam orci ac odio mattis cras integer. Fames mi litora fusce porttitor facilisis. Risus tellus cubilia natoque habitant convallis sem nisl. Pharetra magnis auctor mi neque convallis dictumst interdum. Felis fermentum turpis magna facilisi facilisis euismod dolor. Imperdiet ex risus aptent quam feugiat mus. Leo ad varius tempor; sollicitudin ligula potenti rutrum. Ridiculus sodales molestie ex ridiculus quam tincidunt cursus inceptos.</text>
+
+</body>
+</html>
