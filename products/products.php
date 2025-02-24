3be2eb054cda3a7c83d875789025ec4a187defef
Lan Ngai <b00159127@mytudublin.ie>
2025-02-24 15:36:21 +0000

    created products

--- products/products.php
+++ products/products.php
@@ -1,1 +1,10 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Products Page</title>
+    <link rel="stylesheet" href="css/products.css">
+</head>
+<body></body>
+</html>
